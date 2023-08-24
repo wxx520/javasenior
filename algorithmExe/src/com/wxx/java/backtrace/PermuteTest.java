0f617c42fb55bfd1a3bb48c1d7e18cb42e73423b
unknown <18616380408@163.com>
2023-08-24 16:28:13 +0800

    回溯全排列

--- algorithmExe/src/com/wxx/java/backtrace/PermuteTest.java
+++ algorithmExe/src/com/wxx/java/backtrace/PermuteTest.java
@@ -1,7 +1,6 @@
 package com.wxx.java.backtrace;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author lhbac
@@ -17,17 +16,55 @@ public class PermuteTest {
         List<List<Integer>> ret = new ArrayList<>();
 
         boolean[] used = new boolean[len];
-        
-        dfs(nums, len, 0, new ArrayList<>(), used, ret);
+
+        dfs(nums, len, 0, new ArrayDeque<>(), used, ret);
 
         return ret;
     }
 
-    private void dfs(int[] nums, int len, int depth, List<Integer> path, boolean[] used, List<List<Integer>> res) {
+    private void dfs(int[] nums, int len, int depth, Deque<Integer> path, boolean[] used, List<List<Integer>> ret) {
         if (depth == len) {
-            res.add(new ArrayList<>(path));
+            ret.add(new ArrayList<>(path));
             return;
         }
+        for (int i = 0; i < len; i++) {
+            if (used[i]) {
+                return;
+            }
+            path.offerLast(nums[i]);
+            used[i] = true;
+            dfs(nums, len, depth + 1, path, used, ret);
+            used[i] = false;
+            path.pollLast();
+        }
 
+    }
+
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
     }
 }
